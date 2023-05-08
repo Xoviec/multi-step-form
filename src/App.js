9f12925df0b0b50dbf86cad708170543f7389ec8
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-05-09 00:32:53 +0200

    navbar ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <nav>
-
+        <div className='step-list'>
+          <div className='nav-step'>
+            <button>1</button>
+            <div>
+              <span>STEP 1</span>
+              <p>YOUR INFO</p>
+            </div>
+          </div>
+          <div className='nav-step'>
+            <button>2</button>
+            <div>
+              <span>STEP 2</span>
+              <p>SELECT PLAN</p>
+            </div>
+          </div>
+          <div className='nav-step'>
+            <button>3</button>
+            <div>
+              <span>STEP 3</span>
+              <p>ADD-ONS</p>
+            </div>
+          </div>
+          <div className='nav-step'>
+            <button>4</button>
+            <div>
+              <span>STEP 4</span>
+              <p>SUMMARY</p>
+            </div>
+          </div>
+        </div>
       </nav>
       <container>
       
