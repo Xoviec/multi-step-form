ab9e886dea358b51a5dd86af0d96b15eabdd22de
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-05-08 23:12:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+
+      </nav>
+      <container>
+      
+      </container>
+    </div>
+  );
+}
+
+export default App;
