8ae6d291c97b456124003372d1f7a216b8c66da8
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-05-09 23:36:39 +0200

    ui improveent

--- src/components/SelectPlanForm.jsx
+++ src/components/SelectPlanForm.jsx
@@ -1,8 +1,39 @@
+import arcade from '../assets/images/icon-arcade.svg'
+import advanced from '../assets/images/icon-advanced.svg'
+import pro from '../assets/images/icon-pro.svg'
+
 export const SelectPlanForm = () =>{
 
+
     return(
-        <div>
-            select plan form
+
+        <div className="step-component">
+            <div>
+            <h1>Select your plan</h1>
+                <span>You have the option of monthly or yearly billing.</span>
+                <div className="plans">
+                <div className="plan-card">
+                    <img width="40px" height="40px" src={arcade} alt="" />
+                    <p>Arcade</p>
+                    <span>$9/mo</span>
+                </div>
+                <div className="plan-card">
+                    <img width="40px" height="40px" src={advanced} alt="" />
+                    <p>Advanced</p>
+                    <span>$12/mo</span>
+                </div>
+                <div className="plan-card">
+                    <img width="40px" height="40px" src={pro} alt="" />
+                    <p>Pro</p>
+                    <span>$15/mo</span>
+                </div>
+                </div>
+            </div>
+            <div className="step-component-footer">
+                <button>Go back</button>
+                <button>Next Step</button>
+            </div>
+          
         </div>
     )
 }
--- src/components/UserInfoForm.jsx
+++ src/components/UserInfoForm.jsx
@@ -4,8 +4,32 @@ export const UserInfoForm = () =>{
     return(
 
         <div className="step-component">
-            <h1>Personal Info</h1>
-            <span>Please provide your name, email adress and phone number.</span>
+            <div>
+                <h1>Personal Info</h1>
+                    <span>Please provide your name, email adress and phone number.</span>
+                    <div className="form">
+                        <div className="input-title">
+                            <p>Name</p>
+                            <p className="hidden">This field is required</p>
+                        </div>
+                        <input type="text" placeholder="e.g. Stephen King"/>
+                        <div className="input-title">
+                            <p>Email Address</p>
+                            <p className="hidden">This field is required</p>
+                        </div>                
+                        <input type="text" placeholder="e.g. stephenking@lorem.com"/>
+                        <div className="input-title">
+                            <p>Phone Number</p>
+                            <p>This field is required</p>
+                        </div>                
+                        <input type="text" placeholder="e.g. 732 219 213"/>
+                    </div>
+            </div>
+            <div className="step-component-footer">
+                <div></div>
+                <button>Next Step</button>
+            </div>
+
         </div>
     )
 }
