f07e3835dd7b16003fa6890ec7a9e020467af22f
Adrian Gąsiorek <adriangasiorek@interia.pl>
2023-05-17 23:42:45 +0200

    ux improvement

--- src/App.js
+++ src/App.js
@@ -8,10 +8,35 @@ import { SummaryForm } from './components/SummaryForm';
 function App() {
 
   const [activeStep, setActiveStep] = useState(1)
+  const [changable, setChangable] = useState(true)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: ''
+  });
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+
+
+    if (formData.name && formData.email && formData.phoneNumber) {
+        // Wszystkie pola formularza są wypełnione
+        setChangable(true)
+      } else {
+        setChangable(false)
+      }
+  };
+
 
 
   const changeStep = (num) =>{
 
+        if(changable)
         setActiveStep(activeStep+num)
 
   }
@@ -21,28 +46,28 @@ console.log(activeStep)
     <div className="App">
       <nav>
         <div className='step-list'>
-          <div className={`nav-step ${activeStep === 1 ? `nav-step-active` : ``}`} onClick={()=>(setActiveStep(1))}>
+          <div className={`nav-step ${activeStep === 1 ? `nav-step-active` : ``}`} onClick={()=>(changable ? setActiveStep(1) : null)}>
             <button>1</button>
             <div>
               <span>STEP 1</span>
               <p>YOUR INFO</p>
             </div>
           </div>
-          <div className={`nav-step ${activeStep === 2 ? `nav-step-active` : ``}`}  onClick={()=>(setActiveStep(2))}>
+          <div className={`nav-step ${activeStep === 2 ? `nav-step-active` : ``}`}  onClick={()=>(changable ? setActiveStep(2) : null)}>
             <button>2</button>
             <div>
               <span>STEP 2</span>
               <p>SELECT PLAN</p>
             </div>
           </div>
-          <div className={`nav-step ${activeStep === 3 ? `nav-step-active` : ``}`}  onClick={()=>(setActiveStep(3))}>
+          <div className={`nav-step ${activeStep === 3 ? `nav-step-active` : ``}`}  onClick={()=>(changable ? setActiveStep(3) : null)}>
             <button>3</button>
             <div>
               <span>STEP 3</span>
               <p>ADD-ONS</p>
             </div>
           </div>
-          <div className={`nav-step ${activeStep === 4 ? `nav-step-active` : ``}`}  onClick={()=>(setActiveStep(4))}>
+          <div className={`nav-step ${activeStep === 4 ? `nav-step-active` : ``}`}  onClick={()=>(changable ? setActiveStep(4) : null)}>
             <button>4</button>
             <div>
               <span>STEP 4</span>
@@ -54,7 +79,7 @@ console.log(activeStep)
       <container>
         {
           activeStep === 1 ? 
-            <UserInfoForm changeStep={changeStep}></UserInfoForm>
+            <UserInfoForm changeStep={changeStep} handleInputChange={handleInputChange} formData={formData}></UserInfoForm>
           : activeStep === 2 ? 
             <SelectPlanForm changeStep={changeStep}></SelectPlanForm>
           : activeStep === 3 ? 
--- src/components/UserInfoForm.jsx
+++ src/components/UserInfoForm.jsx
@@ -1,5 +1,20 @@
-export const UserInfoForm = ({changeStep}) =>{
+import { useState } from "react";
 
+export const UserInfoForm = ({changeStep, handleInputChange, formData}) =>{
+
+
+
+
+
+
+    const checkForms = (e) =>{
+
+        // console.log(e.target.name)
+
+        // if(e.target.name)
+        console.log(e)
+
+    }
 
     return(
 
@@ -7,23 +22,23 @@ export const UserInfoForm = ({changeStep}) =>{
             <div>
                 <h1>Personal Info</h1>
                     <span>Please provide your name, email adress and phone number.</span>
-                    <div className="form">
+                    <form className="form" onChange={handleInputChange}>
                         <div className="input-title">
                             <p>Name</p>
-                            <p className="hidden">This field is required</p>
+                            <p className={`${formData.name ? `hidden`: ``}`}>This field is required</p>
                         </div>
-                        <input type="text" placeholder="e.g. Stephen King"/>
+                        <input type="text" placeholder="e.g. Stephen King" name='name' value={formData.name}/>
                         <div className="input-title">
                             <p>Email Address</p>
-                            <p className="hidden">This field is required</p>
+                            <p className={`${formData.email ? `hidden`: ``}`}>This field is required</p>
                         </div>                
-                        <input type="text" placeholder="e.g. stephenking@lorem.com"/>
+                        <input type="text" placeholder="e.g. stephenking@lorem.com" name='email' value={formData.email}/>
                         <div className="input-title">
                             <p>Phone Number</p>
-                            <p>This field is required</p>
+                            <p className={`${formData.phoneNumber ? `hidden`: ``}`}>This field is required</p>
                         </div>                
-                        <input type="text" placeholder="e.g. 732 219 213"/>
-                    </div>
+                        <input type="text" placeholder="e.g. 732 219 213" name='phoneNumber' value={formData.phoneNumber}/>
+                    </form>
             </div>
             <div className="step-component-footer">
                 <div></div>
